d215000af07f86bdb4fd07923339d174bd66ec5c
gloirekiba <gloirekiba21@gmail.com>
2022-08-14 02:46:16 +0200

    💬 update api endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,11 +22,14 @@ const App = () => {
         return;
       }
 
-      const { data } = await axios.get(`http://localhost:5000/api/lyrics`, {
-        params: {
-          query,
-        },
-      });
+      const { data } = await axios.get(
+        `https://open-lyrics.herokuapp.com/api/lyrics`,
+        {
+          params: {
+            query,
+          },
+        }
+      );
 
       setLyrics(data.lyrics);
       setLoading(false);

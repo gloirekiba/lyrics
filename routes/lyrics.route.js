46dd3f6f2949b2fa8b57ae35b21f0b7c4aaa250f
gloirekiba <gloirekiba21@gmail.com>
2022-08-14 11:38:28 +0200

    🥅 improve error handling

--- routes/lyrics.route.js
+++ routes/lyrics.route.js
@@ -8,19 +8,33 @@ const router = express.Router();
 router.use(cors);
 
 router.get("/", async (req, res) => {
-  const { query } = req.query;
-  const Client = new Genius.Client();
-  const searches = await Client.songs.search(query);
+  try {
+    const { query, format } = req.query;
 
-  if (searches.length === 0) {
-    return res.status(404).json({ error: "No results found" });
+    if (!query) {
+      throw new Error("No query provided");
+    }
+
+    const Client = new Genius.Client();
+    const searches = await Client.songs.search(query);
+
+    if (searches.length === 0) {
+      throw new Error("No results found");
+    }
+
+    const firstSong = searches[0];
+    const lyrics = await firstSong.lyrics();
+
+    // if format is raw, return raw lyrics
+    if (format === "raw") {
+      return res.send(lyrics);
+    }
+
+    res.json({ lyrics });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
   }
-
-  const firstSong = searches[0];
-  console.log(searches.length);
-  const lyrics = await firstSong.lyrics();
-
-  res.json({ lyrics });
 });
 
 module.exports = router;

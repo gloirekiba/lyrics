c797c3787e76e9766e97a483311baf11ad88f132
gloirekiba <gloirekiba21@gmail.com>
2022-08-14 03:08:31 +0200

    ✨ update cors method accept

--- middlewares/cors.middleware.js
+++ middlewares/cors.middleware.js
@@ -4,7 +4,7 @@ function cors(req, res, next) {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET");
   next();
 }
 
